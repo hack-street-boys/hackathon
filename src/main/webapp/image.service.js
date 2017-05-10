ca4e773fe01bf8acfbe8ff85c92c02b13f65c8ff
Steve Bott <Steven_Bott@capgroup.com>
2017-05-10 16:25:36 -0700

    Image content meta

--- src/main/webapp/image.service.js
+++ src/main/webapp/image.service.js
@@ -3,12 +3,90 @@
 
 	angular.module("fbApp")
 	.factory('imageService', function($q, $http) {
+		
+		
     return {
     	getImages: function(imageList) {
             var images = imageList;
+    		
+            angular.forEach(imageList, function(url) {
+            	
+            	var metaData = getImageMetaData(url).then(function(metaData) {
+            			console.log(metaData);
+					}, function(error) {
+						console.log(error);
+				});
+            	
+            	
+            	
+            });
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             return images;
-        }
+    	}
     }
+    
+    
+	  function getImageMetaData(imageUrl) {
+          var CV_URL = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBqOBXQ6mRoCDxLyB6gBRyvAeoD_p2FmZ8";
+
+		  var params = {
+				 imageUrl: imageUrl
+		  };
+		  var serializedData = $.param(params);
+		  
+		  return $http({
+			    method: 'POST',
+			    url: '/base64',
+			    data: serializedData,
+			    headers: {
+			        'Content-Type': 'application/x-www-form-urlencoded'
+			    }}).then(function(response) {
+				
+				 var request = {
+						    requests: [{
+						      image: {
+						        content: response.data.value
+						      },
+						      features: [{
+						        type: "LABEL_DETECTION",
+						        maxResults: 200
+						      },
+						      {
+							        type: "WEB_DETECTION",
+							        maxResults: 200
+							  },
+							  {
+							        type: "LANDMARK_DETECTION",
+							        maxResults: 200
+							  }]
+						    }]
+						  };
+				
+				
+				 return $http.post(CV_URL, JSON.stringify(request)).then(function(response) {
+					 console.log(response.data);
+						return response.data;
+					}, function(error) {
+						console.log(error);
+				});
+
+			}, function(error) {
+				console.log(error);
+		});
+		  
+	  }
+    
+    
 });
 
 })();
--- src/main/webapp/results.controller.js
+++ src/main/webapp/results.controller.js
@@ -16,56 +16,7 @@
 		 console.log(user);
 		 console.log(imageList);
 
-		  vm.getImageMetaData = function() {
-			  
-			  var params = {
-					 imageUrl: "https://scontent.xx.fbcdn.net/v/t1.0-9/p720x720/17903680_10154532217178927_5986146878379147924_n.jpg?oh=57980a335802f97fd814f0f1ed7ea5b8&oe=59825973"
-			  };
-			  
-			  var serializedData = $.param(params);
-			  console.log(serializedData);
-			  
-			  //var encodedUrl = encodeURI("https://scontent.xx.fbcdn.net/v/t1.0-9/p720x720/17903680_10154532217178927_5986146878379147924_n.jpg?oh=57980a335802f97fd814f0f1ed7ea5b8&oe=59825973");
-			  $http({
-				    method: 'POST',
-				    url: '/base64',
-				    data: serializedData,
-				    headers: {
-				        'Content-Type': 'application/x-www-form-urlencoded'
-				    }}).then(function(response) {
-					
-					 var request = {
-							    requests: [{
-							      image: {
-							        content: response.data.value
-							      },
-							      features: [{
-							        type: "LABEL_DETECTION",
-							        maxResults: 200
-							      },
-							      {
-								        type: "WEB_DETECTION",
-								        maxResults: 200
-								  },
-								  {
-								        type: "LANDMARK_DETECTION",
-								        maxResults: 200
-								  }]
-							    }]
-							  };
-					
-					
-					 return $http.post(CV_URL, JSON.stringify(request)).then(function(response) {
-							console.log(response);
-						}, function(error) {
-							console.log(error);
-						});
 
-				}, function(error) {
-					console.log(error);
-			});
-			  
-		  }
 			 
 	
 	}
